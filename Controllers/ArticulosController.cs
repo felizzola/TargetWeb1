b7342be9c8f7eb1ca78864684b13b5ee02b798e7
felizzola <63323032+felizzola@users.noreply.github.com>
2021-03-19 19:40:22 -0300

    Add files via upload

---
+++ Controllers/ArticulosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TargetWeb1.Database;
+using TargetWeb1.Models;
+
+namespace TargetWeb1.Controllers
+{
+    public class ArticulosController : Controller
+    {
+        private readonly PortalTargetDbContext _context;
+
+        public ArticulosController(PortalTargetDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Articulos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Articulos.ToListAsync());
+        }
+
+        // GET: Articulos/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articulo = await _context.Articulos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+
+            return View(articulo);
+        }
+
+        // GET: Articulos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articulos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UrlImagen,Codigo,Nombre,Descripcion,StockUnidades,StockEnBulto,PrecioSugMinorista,PrecioSugMayorista,PrecioSugMeLi")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                articulo.Id = Guid.NewGuid();
+                _context.Add(articulo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articulo);
+        }
+
+        // GET: Articulos/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articulo = await _context.Articulos.FindAsync(id);
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articulos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,UrlImagen,Codigo,Nombre,Descripcion,StockUnidades,StockEnBulto,PrecioSugMinorista,PrecioSugMayorista,PrecioSugMeLi")] Articulo articulo)
+        {
+            if (id != articulo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(articulo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticuloExists(articulo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articulo);
+        }
+
+        // GET: Articulos/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articulo = await _context.Articulos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+
+            return View(articulo);
+        }
+
+        // POST: Articulos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var articulo = await _context.Articulos.FindAsync(id);
+            _context.Articulos.Remove(articulo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticuloExists(Guid id)
+        {
+            return _context.Articulos.Any(e => e.Id == id);
+        }
+
+        private Articulo Buscar(int codigo)
+        {
+
+        }
+
+    }
+}
---
+++ Controllers/ArticulosDbConectorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TargetWeb1.Database;
+using TargetWeb1.Models;
+
+namespace TargetWeb1.Controllers
+{
+    public class ArticulosController1 : Controller
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class UserGroupController : ControllerBase
+        {
+            private PortalTargetDbContext PortalTargetDbContext;
+
+            public UserGroupController(PortalTargetDbContext context)
+            {
+                PortalTargetDbContext = context;
+            }
+
+            [HttpGet]
+            public IList<Articulo> Get()
+            {
+                return (this.PortalTargetDbContext.Articulos.ToList());
+            }
+    }
+    }
+}
