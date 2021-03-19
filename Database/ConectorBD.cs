7d9f381ccddb5d372caa28e250c5cdd08bbcbd0f
felizzola <63323032+felizzola@users.noreply.github.com>
2021-03-19 19:39:56 -0300

    Add files via upload

---
+++ Database/ConectorBD.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySqlConnector;
+using System.Data;            //Agregue este tercero
+
+namespace TargetWeb1.Database
+{
+    public class ConectorBD
+    {
+        public static string servidor = "sistemastarget.myddns.me";
+        public static string port = "3306";
+        public static string usuario = "root";
+        public static string pswd = "riachuelo";
+        public static string MYSQLTIMEOUT = "300";
+        //public static string DATABASE = "sistemastarget";
+        public static string DATABASE = "sistemastargettest";
+
+        public static string ararticu = DATABASE + ".xamarinararticu";
+        public static string preciosml = DATABASE + ".xamarinpreciosml";
+        public static string xamarinparamprecioml = DATABASE + ".xamarinparamprecioml";
+
+        MySqlConnection Conexion = new MySqlConnection("server=" + servidor + ";port=" + port + ";database=" + DATABASE + ";" + "uid=" + usuario + ";" + "pwd=" + pswd + ";" + "default command timeout=" + MYSQLTIMEOUT + ";");
+        private MySqlCommandBuilder cmb;
+        public DataSet ds = new DataSet();
+        public DataTable dt = new DataTable();
+        public MySqlDataAdapter da;
+        public MySqlCommand comando;
+        public bool conecto = false;
+        public MySqlTransaction trans;
+
+        public void Conectar()
+        {
+            try
+            {
+                Conexion.Open();
+                //MessageBox.Show("Conectado al servidor");
+                conecto = true;
+
+            }
+            catch (MySqlException ex)
+            {
+                //MessageBox.Show("No se ha podido conectar al servidor " + ex.ToString());
+                conecto = false;
+            }
+            finally
+            {
+                Conexion.Close();
+            }
+
+        }
+
+        public void consultasql(string sql, string tabla)
+        {
+            ds.Tables.Clear();
+            da = new MySqlDataAdapter(sql, Conexion);
+            cmb = new MySqlCommandBuilder(da);
+            da.Fill(ds, tabla);
+        }
+    }
+}
---
+++ Database/PortalTargetDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TargetWeb1.Models;
+
+namespace TargetWeb1.Database
+{
+    public class PortalTargetDbContext : DbContext
+    {
+        public PortalTargetDbContext(DbContextOptions<PortalTargetDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Articulo> Articulos { get; set; }
+  
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Use Fluent API to configure  
+
+            // Map entities to tables  
+            modelBuilder.Entity<Articulo>().ToTable("UserGroups");
+            
+
+            // Configure Primary Keys  
+            modelBuilder.Entity<Articulo>().HasKey(ug => ug.Id).HasName("PK_UserGroups");
+
+            // Configure indexes  
+            modelBuilder.Entity<Articulo>().HasIndex(p => p.Codigo).IsUnique().HasDatabaseName("Idx_Name");
+
+
+            // Configure columns  
+            //  modelBuilder.Entity<Articulo>().Property(ug => ug.Id).HasColumnType("int").UseMySqlIdentityColumn().IsRequired();
+           // modelBuilder.Entity<Articulo>().Property(ug => ug.Id);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.FechaAlta);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.UrlImagen);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.Codigo);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.Nombre);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.Descripcion);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.StockUnidades);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.StockEnBulto);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.PrecioSugMinorista);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.PrecioSugMayorista);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.StockUnidades);
+            modelBuilder.Entity<Articulo>().Property(ug => ug.PrecioSugMeLi);
+
+            // Configure relationships  
+        }
+    }
+}
